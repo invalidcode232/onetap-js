c345ba1aede7de68bf6ad7f9308b27bff1c42c9f
invalidcode232 <78639481+invalidcode232@users.noreply.github.com>
2021-02-21 10:46:12 +0700

    Update Trashtalker.js

--- Trashtalker.js
+++ Trashtalker.js
@@ -22,41 +22,6 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-function delay(callback, milliseconds) {
-    const registeredSymbol = Symbol.for("isDelayRegistered?")
-    const delayCallbacksSymbol = Symbol.for("delayCallbacks")
-
-    if(typeof callback == "function" && typeof milliseconds == "number") {
-        const delaySymbol = Symbol()
-
-        if(!this[delayCallbacksSymbol])
-            this[delayCallbacksSymbol] = {}
-
-        this[delayCallbacksSymbol][delaySymbol] = { callback: callback, milliseconds: milliseconds, start: Date.now() }
-
-        if(this[registeredSymbol] !== true) {
-            this[registeredSymbol] = true
-
-            Cheat.RegisterCallback("Draw", "delay")
-        }
-
-        return delaySymbol
-    } else {
-        const symbols = Object.getOwnPropertySymbols(this[delayCallbacksSymbol])
-
-        for(var i = 0; i < symbols.length; i++) {
-            const delayObject = this[delayCallbacksSymbol][symbols[i]]
-            const difference = Date.now() - delayObject.start
-
-            if(difference >= delayObject.milliseconds) {    
-                delayObject.callback()
-
-                delete this[delayCallbacksSymbol][symbols[i]]
-            }
-        }
-    }
-}
 //#endregion
 
 //#region Replies
